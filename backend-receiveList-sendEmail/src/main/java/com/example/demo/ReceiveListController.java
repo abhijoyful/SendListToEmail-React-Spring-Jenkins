87f6467398b2545c07d20998a01a0f5420513207
Abhishek <abhijoyful@gmail.com>
2022-03-24 09:59:30 +0100

    Adding correct code to the file

--- backend-receiveList-sendEmail/src/main/java/com/example/demo/ReceiveListController.java
+++ backend-receiveList-sendEmail/src/main/java/com/example/demo/ReceiveListController.java
@@ -1,14 +1,21 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@CrossOrigin
+@RestController
 public class ReceiveListController {
 
-	@PostMapping(value = "/takeEmails")
-	public String index(String[] emails) {
+	@PostMapping(value = "/takeEmails",
+			consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+	        produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@ResponseBody
+	public String index(@RequestBody String[] emails) {
 		System.out.println(emails.length);
 		return "success size " + emails.length;
 	}
