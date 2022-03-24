01758b38a93d785816e865a11e960a6789191a38
Abhishek <abhijoyful@gmail.com>
2022-03-24 09:58:22 +0100

    revert commit on wrong file

--- backend-receiveList-sendEmail/src/main/java/com/example/demo/TakeEmailsApplication.java
+++ backend-receiveList-sendEmail/src/main/java/com/example/demo/TakeEmailsApplication.java
@@ -1,23 +1,13 @@
 package com.example.demo;
 
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@CrossOrigin
-@RestController
-public class ReceiveListController {
+@SpringBootApplication
+public class TakeEmailsApplication {
 
-	@PostMapping(value = "/takeEmails",
-			consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-	        produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-	@ResponseBody
-	public String index(@RequestBody String[] emails) {
-		System.out.println(emails.length);
-		return "success size " + emails.length;
+	public static void main(String[] args) {
+		SpringApplication.run(TakeEmailsApplication.class, args);
 	}
 
 }
